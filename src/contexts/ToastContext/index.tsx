ba0a6c09e1ade6f37e8a02ce2624b4184173de53
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-09-04 22:49:08 -0300

    feat: add a provider for the Toast context

--- src/contexts/ToastContext/index.tsx
+++ src/contexts/ToastContext/index.tsx
@@ -1,6 +1,37 @@
-import { createContext } from 'react';
-import { ToastContextValue } from './types';
+import { Toast } from '@/components/Toast';
+import { createContext, useCallback, useState } from 'react';
+import { IToast, ToastContextValue, ToastProviderProps } from './types';
 
 const ToastContext = createContext<ToastContextValue>({} as ToastContextValue);
 
-export { ToastContext };
+function ToastProvider({ children }: ToastProviderProps) {
+	const [toasts, setToasts] = useState<IToast[]>([]);
+
+	const open = useCallback((message: string) => {
+		const newToast: IToast = {
+			id: crypto.randomUUID(),
+			message
+		};
+
+		setToasts(previousToasts => [...previousToasts, newToast]);
+	}, []);
+
+	const close = useCallback((id: string) => {
+		setToasts(previousToasts => previousToasts.filter(toast => toast.id !== id));
+	}, []);
+
+	return (
+		<ToastContext.Provider value={{ open, close }}>
+			{children}
+			{toasts.map(toast => (
+				<Toast
+					key={toast.id}
+					message={toast.message}
+					close={() => close(toast.id)}
+				/>
+			))}
+		</ToastContext.Provider>
+	);
+}
+
+export { ToastContext, ToastProvider };
--- src/contexts/ToastContext/types.ts
+++ src/contexts/ToastContext/types.ts
@@ -1,4 +1,15 @@
+import { ReactNode } from 'react';
+
+export interface IToast {
+	id: string;
+	message: string;
+}
+
 export interface ToastContextValue {
 	open: (message: string) => void;
-	close: (id: number) => void;
+	close: (id: string) => void;
+}
+
+export interface ToastProviderProps {
+	children: ReactNode;
 }
