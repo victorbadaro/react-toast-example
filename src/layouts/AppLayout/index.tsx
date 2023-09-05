12b42939a25b02bc5aec32c993c88c1628e128de
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2023-09-04 23:16:40 -0300

    chore: change app layout style

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,9 +1,21 @@
+import { useToast } from '@/hooks/useToast';
+
 import styles from './styles.module.css';
 
 export function AppLayout() {
+	const { open } = useToast();
+
 	return (
 		<main className={styles['app-layout']}>
-			<h1>React Toast Example</h1>
+			<div>
+				<h1>React Toast Example</h1>
+				<button
+					type="button"
+					onClick={() => open('This is a message!')}
+				>
+					Toast Message
+				</button>
+			</div>
 		</main>
 	);
 }
